76d9a2d98233f094f7f1a87a2e9b0943a74a26c1
Akhil <akhilu5042@gmail.com>
2022-01-08 21:47:08 +0530

    Line Comparision Programs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\n Hello, World!");
+Console.WriteLine("\n Welcome to Line Comparison Computation Program on Master Branch");
+
+Console.WriteLine("Enter X1 value :-");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y1 value :-");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter X2 value :-");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y2 value :-");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\n ---------------------------------------------------------------\n ");
+LineComparisonProblem.UC1.LengthOfLine(X1, X2, Y1, Y2);
+Console.WriteLine("\n ---------------------------------------------------------------\n ");
+LineComparisonProblem.US_2.Equals(X1, X2, Y1, Y2);
+Console.WriteLine("\n ---------------------------------------------------------------\n ");
+LineComparisonProblem.UC_3.Comparision(X1, X2, Y1, Y2);
---
+++ UC1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class UC1
+    {
+        public static void LengthOfLine(int X1, int X2, int Y1, int Y2 )
+        {
+            var distance = Math.Sqrt((Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2)));
+            Console.WriteLine("Length of a line is " + distance);
+        }
+    }
+}
